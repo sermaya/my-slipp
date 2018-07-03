51e8d91514f620969da079a3dbcdd10d8412aca6
happyhouse-PC\happyhouse <sermayar@gmail.com>
2018-07-04 00:07:33 +0900

    controller 추가 및 mstache에 인자 전달

---
+++ src/main/java/net/slipp/myslipp/web/WelcomeController.java
@@ -0,0 +1,16 @@
+package net.slipp.myslipp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping(path = "/helloworld")
+    public String welcome(String name, int age, Model model){
+        System.out.println("name : " + name + " age : " + age);
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "welcome";
+    }
+}
