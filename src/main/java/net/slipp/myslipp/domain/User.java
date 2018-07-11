c689a747bc9c96b2b563fd017e5a9c0a84bedda6
happyhouse-PC\happyhouse <sermayar@gmail.com>
2018-07-11 22:26:07 +0900

    개인정보 수정 기능 구현

--- src/main/java/net/slipp/myslipp/domain/User.java
+++ src/main/java/net/slipp/myslipp/domain/User.java
@@ -19,6 +19,8 @@ public class User {
     private String name;
     private String email;
 
+    public void setId(Long id) { this.id = id; }
+
     public void setUserId(String userId) {
         this.userId = userId;
     }
@@ -33,6 +35,26 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     @Override
--- src/main/java/net/slipp/myslipp/web/UserController.java
+++ src/main/java/net/slipp/myslipp/web/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +22,11 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/form")
+    public String form(){
+        return "/user/form";
+    }
 
     @PostMapping("")
     public String create(User user){
@@ -33,5 +40,12 @@ public class UserController {
     public String list(Model model){
         model.addAttribute("users", userRepository.findAll());
         return "/user/list";
+    }
+
+    @GetMapping("/{id}/form")
+    public String updateForm(@PathVariable Long id, Model model){
+        User user = userRepository.getOne(id);
+        model.addAttribute("user", user);
+        return "/user/updateForm";
     }
 }
