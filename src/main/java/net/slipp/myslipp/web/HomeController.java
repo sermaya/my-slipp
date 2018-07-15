9143e371d5dd2896ce79c143cafc78bb6c10ac67
happyhouse-PC\happyhouse <sermayar@gmail.com>
2018-07-15 20:42:26 +0900

    질문하기 목록 기능 구현

--- src/main/java/net/slipp/myslipp/web/HomeController.java
+++ src/main/java/net/slipp/myslipp/web/HomeController.java
@@ -1,12 +1,21 @@
 package net.slipp.myslipp.web;
 
+import net.slipp.myslipp.domain.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    QuestionRepository questionRepository;
+
     @GetMapping("")
-    public String home(){
+    public String home(Model model){
+
+        model.addAttribute("questions", questionRepository.findAll());
+
         return "index";
     }
 }
